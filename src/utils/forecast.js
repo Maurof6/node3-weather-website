fc862f2fbaaf9506645f3a413d8556866982ed4b
Mauricio Quezada Celi <sirarguer@gmail.com>
2021-10-15 16:16:33 -0500

    Add humididty to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, lon, callback) => {
             callback('Unable to find location!', undefined)
         } else {
             callback(undefined,
-                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. There is a wind speed of ' + body.current.wind_speed + ' kilometers per hour.')
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degress out.'+' the humidity is '+ body.current.humidity +'%. There is a wind speed of ' + body.current.wind_speed + ' kilometers per hour.')
         }
     })
 }
